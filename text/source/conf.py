ffae5923113f0fa2c616a7d029144e4656a4a86e
Michael M. Tiller <michael.tiller@gmail.com>
2013-11-12 10:19:22 -0500

    Improved look

--- text/source/conf.py
+++ text/source/conf.py
@@ -13,6 +13,8 @@
 
 import sys, os
 
+sys.path.append(os.path.abspath('_themes'))
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -97,7 +99,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
+html_theme = 'celery'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -106,6 +109,7 @@ html_theme = 'default'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
+html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
