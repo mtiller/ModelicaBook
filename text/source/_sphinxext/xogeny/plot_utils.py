4718788275afc2fb41db16d9fa5f428dbcddb555
Michael Tiller <michael.tiller@gmail.com>
2013-11-13 18:03:38 -0500

    Excellent progress
    Now I need to go through all the existing plots and make them acceptable.

--- text/source/_sphinxext/xogeny/plot_utils.py
+++ text/source/_sphinxext/xogeny/plot_utils.py
@@ -25,14 +25,59 @@ import os
 def render_twoup_plot(name, spec1, spec2):
     pass
 
-def render_comp_plot(name1, vars1, name2, vars2):
-    raise BaseException("Need to implement handling of comparison plots")
-
-def render_simple_plot(name, vars, title=None, legloc="lower right"):
+def render_comp_plot(name1, vars1, name2, vars2, title, legloc, ylabel):
     import matplotlib.pyplot as plt
     import math
 
-    styles = [""]
+    if len(vars1)!=len(vars2):
+        raise BaseException("Mismatch in variable arrays, %d vs %s" % (len(vars1), len(vars2)))
+
+    # Location of results (relative to extention directory)
+    resdir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                          "..", "..", "..", "results"))
+    fn1 = os.path.join(resdir, name1+"_res.mat")
+    res1 = DyMatFile(fn1)
+
+    fn2 = os.path.join(resdir, name2+"_res.mat")
+    res2 = DyMatFile(fn2)
+
+    fig, ax = plt.subplots()
+
+    var0name = vars1[0]["name"]
+
+    t1 = res1.abscissa(vars1[0]["name"], valuesOnly=True)
+    t2 = res2.abscissa(vars2[0]["name"], valuesOnly=True)
+
+    for i in range(0,len(vars1)):
+        v1 = vars1[i]
+        v2 = vars2[i]
+        x1 = res1.data(v1["name"])
+        x2 = res2.data(v2["name"])
+        l1 = v1["legend"]
+        l2 = v2["legend"]
+
+        if len(x1)==2:
+            x1 = [x1[0]]*len(t1)
+        if len(x2)==2:
+            x2 = [x2[0]]*len(t2)
+
+        ax.plot(t1, x1, "-", label=l1)
+        ax.plot(t2, x2, "-.", label=l2)
+
+    legend = ax.legend(loc=legloc, shadow=True)
+
+    if title==None:
+        title = name1.replace("_", ".")
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel('Time [s]')
+
+    plt.show()
+
+
+def render_simple_plot(name, vars, title, legloc, ylabel):
+    import matplotlib.pyplot as plt
+    import math
 
     # Location of results (relative to extention directory)
     resdir = os.path.abspath(os.path.join(os.path.dirname(__file__),
@@ -65,7 +110,7 @@ def render_simple_plot(name, vars, title=None, legloc="lower right"):
     if title==None:
         title = name.replace("_", ".")
     plt.title(title)
-    #plt.ylabel(var)
+    plt.ylabel(ylabel)
     plt.xlabel('Time [s]')
 
     plt.show()
