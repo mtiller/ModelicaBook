e82709fcfaebd6ca262f4be433940e379e2b0aee
Michael Tiller <michael.tiller@gmail.com>
2014-02-18 12:33:17 -0500

    Latest look and feel

--- text/source/_themes/xogeny-semantic/static/xogeny.js
+++ text/source/_themes/xogeny-semantic/static/xogeny.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     /* If this is a normal browser, show sidebar */
     $("#toc-sidebar").sidebar('show');
     /* If mobile, hide it and let people open it with a button */
+    if ($(window).width()<1000) {
+	$("#toc-sidebar").sidebar('hide');
+    }
 
     /* Make the thumb button open and close the TOC */
     $('#toc-sidebar').sidebar('attach events', '#thumb', 'toggle');
