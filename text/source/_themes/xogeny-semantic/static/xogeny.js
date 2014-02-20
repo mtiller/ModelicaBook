7bfb889cc455c5fcab74744d6d09c4ff26c4cb72
Michael Tiller <michael.tiller@gmail.com>
2014-02-19 21:03:38 -0500

    Got a reasonable start on the vendor image rotation stuff
    I'm still a bit concerned that clicking on a thumbnail will make it difficult to show the
    profile for that sponsor.  Much easier would be hover gives profile in a popup and
    clicking goes to their page and the animation just works completely independently.

--- text/source/_themes/xogeny-semantic/static/xogeny.js
+++ text/source/_themes/xogeny-semantic/static/xogeny.js
@@ -3,16 +3,17 @@ $(document).ready(function() {
     $('#toc-sidebar').sidebar('attach events', '#thumb', 'toggle');
 
     var frames = [["s1", "s2"], ["b1", "b2", "b3"], ["g1"]];
-    var showtime = 3000;
+    var showtime = 6000;
 
     var showFrame = function(n, cb) {
 	console.log("Showing frame "+n);
 	for(var i=0;i<frames[n].length;i++) {
 	    $("#"+frames[n][i]).transition({
-		animiation: "fade in",
-		complete: cb
+		animation: "fade in"
 	    });
+	    $("#"+frames[n][i]+"i").addClass("svis");
 	}
+	if (cb) { setTimeout(cb, 300); }
     };
 
     var hideFrame = function(n, cb) {
@@ -20,9 +21,10 @@ $(document).ready(function() {
 	for(var i=0;i<frames[n].length;i++) {
 	    $("#"+frames[n][i]).transition({
 		animation: "fade out",
-	        complete: cb
 	    });
+	    $("#"+frames[n][i]+"i").removeClass("svis");
 	}
+	if (cb) { setTimeout(cb, 300); }
     };
 
     var transFrame = function(n1, n2) {
@@ -30,17 +32,25 @@ $(document).ready(function() {
     };
 
     var animateLogos = function() {
+	console.log("show frame "+0);
 	showFrame(0);
-	setTimeout(function () { transFrame(0, 1); }, showtime);
-	setTimeout(function () { transFrame(1, 2); }, 2*showtime);
-	setTimeout(function () { hideFrame(2, animateLogos); }, 3*showtime);
+	for(var i=0;i<frames.length-1;i++) {
+	    (function() {
+		var j = i;
+		console.log("transframe "+j+" -> "+(j+1));
+		setTimeout(function () { transFrame(j, j+1); }, (j+1)*showtime);
+	    })();
+	}
+	console.log("hide frame "+(frames.length-1));
+	setTimeout(function () { hideFrame(frames.length-1, animateLogos); },
+		   frames.length*showtime);
     };
 
-    animateLogos();
-
-    /* Start animation of sponsors */
-    $(".logo").transition({
-	animation: "fade"
-	//complete: animateLogos
-    });
+    if (true) {
+	/* Start animation of sponsors */
+	$(".logo").transition({
+	    animation: "fade"
+	});
+	setTimeout(animateLogos, 300);
+    }
 });
