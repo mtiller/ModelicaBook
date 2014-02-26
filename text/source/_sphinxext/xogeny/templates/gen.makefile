cdbfbaa870ce43902a92853548c1667dc2d1511b
Michael M. Tiller <michael.tiller@gmail.com>
2014-02-26 08:29:13 -0500

    Simulations are running

--- text/source/_sphinxext/xogeny/templates/gen.makefile
+++ text/source/_sphinxext/xogeny/templates/gen.makefile
@@ -4,17 +4,25 @@ results_files: {%- for res in results %} {{res}}_res.mat {% endfor %}
 
 js_files: {%- for res in results %} {{res}}.js {% endfor %}
 
-json_files: {%- for res in results %} {{res}}.json {% endfor %}
+json_files: {%- for res in results %} json/{{res}}.json {% endfor %}
+
+json:
+	-mkdir json
+
+js:
+	-mkdir js
 
 {% for res in results %}
 {{res}}_res.mat {{res}}_init.xml: {{results[res]["path"]}}
 	omc {{res}}.mos
 
-{{res}}.json: {{res}}_init.xml ../tojson.py
-	../tojson.py {{res}}_init.xml {{res}}.json
+json/{{res}}.json: json {{res}}_init.xml ../tojson.py
+	../tojson.py {{res}}_init.xml json/{{res}}.json
 
-{{res}}.js:  {{results[res]["path"]}}
+js/{{res}}.js:  js {{results[res]["path"]}}
 	omc {{res}}-js.mos
+	-mv {{res}}.node.js js/{{res}}.node.js
+	mv {{res}}.js js/{{res}}.js
 {% endfor %}
 
 tidy:
--- text/source/_themes/xogeny-semantic/static/xogeny.js
+++ text/source/_themes/xogeny-semantic/static/xogeny.js
@@ -8,21 +8,49 @@ $(document).ready(function() {
 
     var source = $("#itemplate").html();
     var it = Handlebars.compile(source);
+    var worker = null;
 
+    var runsim = function(jobj, id) {
+	console.log("Whee! let's simulate "+id+"!");
+	if (worker!=null) worker.terminate();
+	worker = new Worker("/_static/js/"+id+".js");
+	worker.addEventListener("error", function(event) {
+	    console.log("Error from worker:");
+	    console.log(event);
+	});
+	worker.addEventListener("message", function(event) {
+	    var data = event.data;
+	    console.log("Simulation status: "+data.status);
+	    var x = $.csv.toArrays(event.data.csv,
+				   {onParseValue: $.csv.hooks.castToScalar})
+	    console.log("x = ");
+	    console.log(x);
+	    console.log(event);
+	});
+	console.log("Starting simulation");
+	var stopTime = parseFloat(jobj.experiment.stopTime);
+	var step = stopTime/100;
+	worker.postMessage({"basename": "FO",
+			    "stopTime": String(stopTime),
+			    "tolerance": "1e-03",
+			    "stepSize": String(step)})
+    };
+    
     var fixfig = function(p, was, id, jobj) {
 	var context = {"image": was, "id": id, "model": jobj};
 	var newt = it(context);
 	$(p).html(newt);
 	$(".ui.accordion").accordion();
 	$("#sim-button-"+id).click(function () {
-	    console.log("Whee! let's simulate it!");
+	    runsim(jobj, id);
 	});
     }
 
     $(".interactive").each(function(i, elem) {
 	var p = $(elem).parent();
+	var src = $(elem).attr("src");
 	var was = $(p).html();
-	var id = "SV2";
+	var id = src.split("/").pop().split(".")[0];
 	$.ajax({
 	    dataType: "json",
 	    url: "/_static/json/"+id+".json",
