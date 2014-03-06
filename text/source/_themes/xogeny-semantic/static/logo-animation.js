99338a68e194a497c9a390832e98a8903f6d5801
Michael M. Tiller <michael.tiller@gmail.com>
2014-03-06 18:38:50 -0500

    Profiles now pop up due to hover events

--- text/source/_themes/xogeny-semantic/static/logo-animation.js
+++ text/source/_themes/xogeny-semantic/static/logo-animation.js
@@ -1,50 +1,24 @@
 $(document).ready(function() {
-    var thumbnails = $(".thumbnail");
-    console.log("# of thumbnails: "+thumbnails.length);
-    thumbnails.popup({
-	"position": "bottom right",
-	"inline": true,
-	"preserve": true
-    });
-
     var groups = ["CyDesign", "Wolfram", "Modelon", "Maplesoft", "DS",
 		  "Ricardo", "ITI", "GlobalCrown", "Siemens",
 		  "ST", "OSMC", "dofware", "BauschGall", "TUHH", "Schlegel"];
 
-    function shuffle(array) {
-	var currentIndex = array.length;
-	var temporaryValue;
-	var randomIndex;
-
-	// While there remain elements to shuffle...
-	while (0 !== currentIndex) {
-
-	    // Pick a remaining element...
-	    randomIndex = Math.floor(Math.random() * currentIndex);
-	    currentIndex -= 1;
-
-	    // And swap it with the current element.
-	    temporaryValue = array[currentIndex];
-	    array[currentIndex] = array[randomIndex];
-	    array[randomIndex] = temporaryValue;
-	}
-
-	return array;
-    }
-
-    var order = shuffle(groups);
-    var next = 0;
-
-    var shownext = function() {
+    var showsponsor = function(id) {
 	$(".sentry").hide();
 	$(".thumbnail").removeClass("thumbshadow");
-	$("#sentry-"+order[next]).show();
-	$("#thumbnail-"+order[next]).addClass("thumbshadow");
-	console.log("Showing "+order[next]);
-	if (next<order.length-1) next = next + 1;
-	else next = 0;
+	$("#sentry-"+id).show();
+	$("#thumbnail-"+id).addClass("thumbshadow");
     };
 
-    shownext();
-    //setInterval(shownext, 5000);
+    for(var i=0;i<groups.length;i++) {
+	(function() {
+	    var j = i;
+	    $("#thumbnail-"+groups[j]).hover(function () {
+		console.log("Show "+groups[j]);
+		showsponsor(groups[j]);
+	    });
+	})();
+    }
+
+    $(".sentry").hide();
 });
