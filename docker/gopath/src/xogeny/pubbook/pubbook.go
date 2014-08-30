454ce6a43473fc839f458d3612520a54a76f5abf
Michael Tiller <michael.tiller@gmail.com>
2014-08-30 13:23:01 -0400

    Added error output and fixed some issues with how git was working

--- docker/gopath/src/xogeny/pubbook/pubbook.go
+++ docker/gopath/src/xogeny/pubbook/pubbook.go
@@ -4,6 +4,7 @@ import "os"
 import "fmt"
 import "log"
 import "path"
+import "bytes"
 import "os/exec"
 
 import hs "github.com/xogeny/go-hooksink"
@@ -12,13 +13,26 @@ type Builder struct {
 	Debug bool
 }
 
-func git(args...string) error {
+func git(dir string, args...string) error {
 	log.Printf("Running %v...", args);
 
+	ebuf := []byte{};
+	obuf := []byte{};
+
+	stderr := bytes.NewBuffer(ebuf);
+	stdout := bytes.NewBuffer(obuf);
+
 	cmd := exec.Command("git", args...)
+	cmd.Dir = dir;
+	cmd.Stderr = stderr;
+	cmd.Stdout = stdout;
 	err := cmd.Run()
 	if err != nil {
 		log.Printf("Error: %s", err.Error());
+		log.Print("=== Output ===");
+		log.Print(stdout.String());
+		log.Print("=== Error ===");
+		log.Print(stderr.String());
 		return err;
 	} else {
 		log.Printf("...successful");
@@ -27,12 +41,24 @@ func git(args...string) error {
 }
 
 func make(dir string, targets...string) error {
+	ebuf := []byte{};
+	obuf := []byte{};
+
+	stderr := bytes.NewBuffer(ebuf);
+	stdout := bytes.NewBuffer(obuf);
+
 	cmd := exec.Command("make", targets...);
+	cmd.Stderr = stderr;
+	cmd.Stdout = stdout;
 	cmd.Dir = path.Join(dir, "text");
 	log.Printf("Running initial make: %v...", targets);
 	err := cmd.Run()
 	if err != nil {
 		log.Printf("Error running initial make '%v': %s", targets, err.Error());
+		log.Print("=== Output ===");
+		log.Print(stdout.String());
+		log.Print("=== Error ===");
+		log.Print(stderr.String());
 		return err;
 	} else {
 		log.Printf("...successful");
@@ -56,27 +82,27 @@ func (b Builder) Push(msg hs.HubMessage) {
 	// TODO: Have a query parameter to clear cache
 
 	if (exists) {
-		err = git("fetch", "origin");
+		err = git(dir, "fetch", "origin");
 		if (err!=nil) { return; }
 	} else {
-		err = git("clone", url, dir);
+		err = git(dir, "clone", url, dir);
 		if (err!=nil) { return; }
 	}
 
 	/* Repo checkout correct ref */
-	err = git("checkout", ref)
+	err = git(dir, "checkout", ref)
 	if err != nil { return; }
 
 	bucket := fmt.Sprintf("S3BUCKET=dev.book.xogeny.com/%s", user);
 	if (!exists) {
 		// If it didn't already exist, we need to run some make targets
 		/* Run make */
-		make(dir, "specs", "results");
+		make(dir, "specs");
 		if (err!=nil) { return; }
 	}
 
 	/* Run make */
-	make(dir, target1, target2, bucket);
+	make(dir, "results", target1, target2, bucket);
 	if err != nil {	return; }
 
 	log.Printf("Make ran!");
