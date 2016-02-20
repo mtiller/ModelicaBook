d3a6aaac70e3989b690acb7ed75c2ceec3c14a37
谢东平 (XIE Dongping) <dongping.xie.tud@gmail.com>
2016-02-20 16:04:42 +0100

    Circle CI: Specify bucket with env variable

--- docker/build_with_docker.sh
+++ docker/build_with_docker.sh
@@ -3,4 +3,6 @@ image_name="xiedongping/book-mbe"
 echo $image_name
 
 sudo docker build --no-cache -t $image_name MBE
-sudo docker run -t -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" -e "AWS_SECRET_KEY=$AWS_SECRET_KEY" $image_name
+sudo docker run -t -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
+    -e "AWS_SECRET_KEY=$AWS_SECRET_KEY" \
+    -e "S3BUCKET=$S3BUCKET" $image_name
