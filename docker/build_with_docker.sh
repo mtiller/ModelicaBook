8893e199ebe449d8e388407dd3aa8fbcdf2f243f
Michael Tiller <michael.tiller@gmail.com>
2017-11-03 14:33:49 -0400

    All-in-one Docker file
    (soon to be removed)

--- docker/build_with_docker.sh
+++ docker/build_with_docker.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
-image_name="xiedongping/book-mbe"
+image_name="mtiller/book-mbe"
 echo $image_name
 
 if [ -z "${S3BUCKET}" ]; then 
     S3BUCKET='book.xogeny.com'
 fi
 
-sudo docker pull xiedongping/book-py
-sudo docker build --no-cache -t $image_name MBE
-sudo docker run -t -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
-    -e "AWS_SECRET_KEY=$AWS_SECRET_KEY" \
-    -e "S3BUCKET=$S3BUCKET" $image_name
+# TODO: In a CI system, we'll need to do this...
+# sudo docker pull mtiller/book-py
+
+# TODO: I don't think building fresh is the way to go.  Better to pull
+# the built image so we know exactly what image will be used.
+#sudo docker build --no-cache -t $image_name MBE
+
+docker run -v ..:/opt/MBE/ModelicaBook -t $image_name
+#sudo docker run -t -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
+#    -e "AWS_SECRET_KEY=$AWS_SECRET_KEY" \
+#    -e "S3BUCKET=$S3BUCKET" $image_name
